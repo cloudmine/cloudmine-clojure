f3b2d8802faf5e8982908118bd9130389abeae6e
Derek Mansen <dmansen@rjmetrics.com>
2011-10-30 15:08:10 -0400

    give response bodies directly

--- src/cloudmine.clj
+++ src/cloudmine.clj
@@ -4,6 +4,8 @@
   (:require [clojure.core :as core])
   (:require [clj-http.client :only (post)  :as client]))
 
+(def cloudmine-api-url "https://api.cloudmine.me/v1/app/")
+
 (defn response-body
   [r]
   (-> r :body read-json))
@@ -21,33 +23,33 @@
    be a clojure keyword, data should be a native clojure data
    structure."
   [{cm-app-id :cm-app-id cm-api-key :cm-api-key} data]
-  (client/put (str "https://api.cloudmine.me/v1/app/"
-                     cm-app-id
-                     "/text")
-                {:body (json-str data)
-                 :headers {"X-Cloudmine-ApiKey" cm-api-key}
-                 :content-type :application/json}))
+  (response-body (client/put (str cloudmine-api-url
+                                  cm-app-id
+                                  "/text")
+                             {:body (json-str data)
+                              :headers {"X-Cloudmine-ApiKey" cm-api-key}
+                              :content-type :application/json})))
 
 (defn get
   "Queries cloudmine by keys"
   [{cm-app-id :cm-app-id cm-api-key :cm-api-key} & keys]
-  (client/get (str
-               "https://api.cloudmine.me/v1/app/"
-               cm-app-id
-               "/text?keys="
-               (keys-string (map name keys)))
-              {:headers {"X-Cloudmine-ApiKey" cm-api-key}
-               :content-type :application/json
-               :accepts :application/json}))
+  (response-body (client/get (str
+                              cloudmine-api-url
+                              cm-app-id
+                              "/text?keys="
+                              (keys-string (map name keys)))
+                             {:headers {"X-Cloudmine-ApiKey" cm-api-key}
+                              :content-type :application/json
+                              :accepts :application/json})))
 
 (defn query
   "Queries cloudmine using search query language"
   [{cm-app-id :cm-app-id cm-api-key :cm-api-key} query]
-  (client/get (str
-               "https://api.cloudmine.me/v1/app/"
-               cm-app-id
-               "/search?q="
-               query)
-              {:headers {"X-Cloudmine-ApiKey" cm-api-key}
-               :content-type :application/json
-               :accepts :application/json}))
+  (response-body (client/get (str
+                              cloudmine-api-url
+                              cm-app-id
+                              "/search?q="
+                              query)
+                             {:headers {"X-Cloudmine-ApiKey" cm-api-key}
+                              :content-type :application/json
+                              :accepts :application/json})))
